155024cf9e372f2720d6def3faf6ffdcc74b2d7e
gjm112 <gjm112@gmail.com>
2017-04-17 22:18:24 -0500

    updates

--- server.R
+++ server.R
@@ -1,2 +1,23 @@
 shinyServer(function(input, output) {
-})
+
+  
+  
+  output$text1 <- renderText({ 
+    paste("You have selected", input$num)
+  })
+  
+  output$text2 <- renderText({ 
+    paste("You have selected", input$text)
+  })
+  
+  #################
+  #Render functions
+  #################
+  # renderImage	images (saved as a link to a source file)
+  # renderPlot	plots
+  # renderPrint	any printed output
+  # renderTable	data frame, matrix, other table like structures
+  # renderText	character strings
+  # renderUI	a Shiny tag object or HTML
+  
+  })
